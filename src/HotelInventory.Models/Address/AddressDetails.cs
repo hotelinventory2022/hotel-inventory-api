255ce87b0f433b71937bb2ba878a1210ed5a2e38
Hiranmoy Naskar <hotelinventory2022@gmail.com>
2022-02-27 03:34:18 +0530

    added models for property upload

---
+++ src/HotelInventory.Models/Address/AddressDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace HotelInventory.Models.Address
+{
+    public class AddressDetails
+    {
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Pincode { get; set; }
+        #region owner upload only
+        public List<int> AreaIds { get; set; }
+        public string Landmark { get; set; }
+        #endregion
+    }
+}
